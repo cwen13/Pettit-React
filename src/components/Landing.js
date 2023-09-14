05053a1deb4e0f7ea44a4930d14f97611ddccd7a
cwen13 <cwenrich13@gmail.com>
2023-09-14 16:49:55 -0400

    setting up the pages and getting links started

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -1,16 +1,11 @@
 import React from "react";
-import { Link } from "react-router-dom";
 
-import Navbar from "./Navbar";
 import Preview from "./Preview";
 
 class Landing extends React.Component {
   render() {
-    console.log("building the previews");
-    console.log(this.props.pets)
     return (
       <>
-	<Navbar />
 	<p> This is where ther will be anaimals</p>
 	<ul className="petList"> 
 	{Object.keys(this.props.pets).map(key => (
